5d1086079fdbeb091f1288993061f346f10ff896
Waleed Azhar <waleedazhar@locals-mbp.dayforce.com>
2017-08-17 10:06:04 -0400

    Initial Commit

---
+++ Price Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Price Tracker
+//
+//  Created by Waleed Azhar on 8/17/17.
+//  Copyright © 2017 Waleed Azhar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
